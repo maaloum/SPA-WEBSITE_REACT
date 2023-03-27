67cc2ba7ab78525e768992d4291f010d8e9e478c
Maaloum <50062179+maaloum@users.noreply.github.com>
2023-03-27 18:52:34 +0300

    about styling

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -5,7 +5,7 @@ export default function About() {
   return (
     <div class=" container">
         <div class="left ">
-            <div className='content'>
+            <div className='cont'>
                 <h1>The Beauty is about 
                 being Comfortable
                 in your own skin!</h1>
@@ -13,10 +13,10 @@ export default function About() {
             <div className='subcontent'>
                 <p>There are many variation of passages are Ipsum available, majority have suffered alteration in some form.</p>
             </div>
-            <div className='pictures'>
-                <img src="./assests/pic1.png" alt="pic1"/>
-                <img src="./assests/pic2.png" alt="pic2"/>
-                <img src="./assests/pic3.png" alt="pic3"/>
+            <div className='pictures px-4'>
+                <img src="./assests/img.png" alt="pic1"/>
+                <img src="./assests/img.png" alt="pic2"/>
+                <img src="./assests/img.png" alt="pic3"/>
             </div>
         </div>
         <div className='right '>
